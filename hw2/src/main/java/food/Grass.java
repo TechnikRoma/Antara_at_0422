806e51a3c6e0f04d7fbe343f79089b02cf276163
Roman.Rogozhnikov <roman.rogozhnikov@antara-services.ru>
2022-06-05 15:59:29 +0300

    Added java homework 2

---
+++ hw2/src/main/java/food/Grass.java
@@ -0,0 +1,11 @@
+package food;
+
+public class Grass extends Food {
+
+    public Grass(int foodAmount, String foodName) {
+        super(foodAmount, foodName);
+        this.foodType = "grass";
+    }
+
+
+}
